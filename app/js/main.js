86627c0a1e01a05b596a9ec8489f278f5884afd1
Evgeniy <emashkovtsev@gmail.com>
2021-04-26 00:26:19 +0300

    desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,16 @@ $(function () {
         fade: true,
         autoplay: true
     });
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false,
+    });
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="arrow"></button>'
+    });
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.living'
