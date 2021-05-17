4427110b8e48d668176f4e5e982121d563701815
Evgeniy <emashkovtsev@gmail.com>
2021-05-17 18:16:26 +0300

    gh-pages

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,12 @@ $(function () {
     $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
+    $('.header__btn-menu').on('click', function () {
+        $('.menu').toggleClass('menu--open');
+    });
+    if($(window).width() < 651) {
+        $('.works-path__item--meas').appendTo($('.works-path__items-box'));
+    }
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -17,6 +23,43 @@ $(function () {
         slidesToScroll: 10,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+              breakpoint: 1700,
+              settings: {
+                slidesToShow: 8,
+                slidesToScroll: 8,
+              }
+            },
+            {
+              breakpoint: 1511,
+              settings: {
+                slidesToShow: 6,
+                slidesToScroll: 6,
+              }
+            },
+            {
+              breakpoint: 1180,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+              }
+            },
+            {
+               breakpoint: 750,
+               settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+               }
+            },
+            {
+               breakpoint: 540,
+               settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+               }
+            }
+        ]
     });
     $('.article-slider__box').slick({
         prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow"></button>',
